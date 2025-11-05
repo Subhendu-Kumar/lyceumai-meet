fec0e15944b7f190938de0412dae57cf7f3a4182
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2025-11-05 17:01:13 +0530

    fixes

--- components/EndCallButton.tsx
+++ components/EndCallButton.tsx
@@ -22,7 +22,7 @@ const EndCallButton = () => {
     <Button
       onClick={async () => {
         await call.endCall();
-        router.push("/");
+        router.replace("/");
       }}
       className="bg-red-600 hover:bg-red-700"
     >
